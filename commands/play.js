4b17396d7afcccb246b490e9d790aeec4e86ca46
Jack Sibley <jacks@sibley.com.au>
2023-02-19 17:10:24 +1000

    got audio playback working

--- commands/play.js
+++ commands/play.js
@@ -1,10 +1,95 @@
 const { SlashCommandBuilder } = require("discord.js");
+const ytdl = require("ytdl-core");
+const ffmpeg = require("ffmpeg-static");
+const { google } = require("googleapis");
+const { youtubeKey, guildId } = require("../config.json");
+const {
+  AudioPlayerStatus,
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+} = require("@discordjs/voice");
+
+const youtube = google.youtube({
+  version: "v3",
+  auth: youtubeKey,
+});
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("play")
-    .setDescription("Plays music from search query"),
-  async execute(interaction) {
-    await interaction.reply("Pong!");
+    .setDescription("Plays music from search query")
+    .addStringOption((option) =>
+      option.setName("input").setDescription("Song Title").setRequired(true)
+    ),
+  async execute(interaction, client) {
+    const searchQuery = interaction.options.getString("input");
+    //get voice channel ID
+    const voiceChannelId = interaction.guild.members.cache.get(
+      interaction.member.user.id
+    ).voice.channelId;
+
+    //create audio player
+    const player = createAudioPlayer();
+
+    player.on(AudioPlayerStatus.Playing, () => {
+      console.log("The audio player has started playing!");
+    });
+
+    player.on("error", (error) => {
+      console.error(`Error: ${error.message} with resource`);
+    });
+
+    //get audio from youtube
+    try {
+      const searchResults = await youtube.search.list({
+        part: "id",
+        q: searchQuery,
+        type: "video",
+        maxResults: 1,
+      });
+
+      if (!searchResults.data.items || searchResults.data.items.length === 0) {
+        interaction.reply(
+          "Sorry, I could not find any videos with that search query."
+        );
+        return;
+      }
+
+      const videoId = searchResults.data.items[0].id.videoId;
+      const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+
+      // Join the voice channel if the user is in one
+      if (voiceChannelId) {
+        //create the connection to voice channel
+        const connection = joinVoiceChannel({
+          channelId: voiceChannelId,
+          guildId: guildId,
+          adapterCreator: interaction.guild.voiceAdapterCreator,
+        });
+
+        const stream = ytdl(videoUrl, { filter: "audioonly" });
+
+        //create and play audio
+        const resource = createAudioResource(stream);
+        player.play(resource);
+
+        interaction.reply("created voice connection");
+
+        const subscription = connection.subscribe(player);
+
+        if (subscription) {
+          // Unsubscribe after 5 seconds (stop playing audio on the voice connection)
+          setTimeout(() => subscription.unsubscribe(), 30_000);
+        }
+      } else {
+        interaction.reply("You need to join a voice channel first!");
+      }
+    } catch (error) {
+      console.error(error);
+      interaction.reply(
+        "An error occurred while processing your request. Please try again later."
+      );
+    }
   },
 };
--- index.js
+++ index.js
@@ -2,12 +2,29 @@
 const fs = require("node:fs");
 const path = require("node:path");
 const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
-const { token, YTKEY } = require("./config.json");
+const { token } = require("./config.json");
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+});
 
 client.commands = new Collection();
+
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
@@ -26,33 +43,6 @@ for (const file of commandFiles) {
     );
   }
 }
-
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
-  }
-});
 
 // Log in to Discord with your client's token
 client.login(token);
